005d95f24e73c5f0d47b52e1066aad33458fb4e8
THIAGO GUEDES <THIAGO GUEDES>
2017-07-08 18:02:01 -0300

    servico login

--- src/app/pages/login/login-service.service.ts
+++ src/app/pages/login/login-service.service.ts
@@ -1,8 +1,20 @@
+import { HttpModule, Http } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { HttpEndPointsService } from "app/providers/http-end-points.service";
 
 @Injectable()
 export class LoginServiceService {
 
-  constructor() { }
+  constructor(private http: Http, private httpUtil: HttpEndPointsService) { }
+
+  logar() {
+    let params = JSON.stringify(
+			{ "username": 'usuario', "password": 'senha' });
+
+      /*return this.http.post(this.httpUtil.(this.loginUrl), params, 
+						this.httpUtil.headers())
+	                .map(this.httpUtil.extrairDados)
+	                .catch(this.httpUtil.processarErros);*/
+  }
 
 }
